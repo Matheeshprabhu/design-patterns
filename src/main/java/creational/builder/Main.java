4bab5f9cd585aa5a8442401ef529f1e002a1f963
Matheesh <matheeshprabhu123@gmail.com>
2021-12-19 01:28:44 +0530

    Adding Builder pattern real world implementation

--- src/main/java/creational/builder/Main.java
+++ src/main/java/creational/builder/Main.java
@@ -6,8 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        UserDTO userDTO = directBuild(new UserWebDTOBuilder(), createUser());
-
+        UserDTO userDTO = directBuild(UserWebDTO.getBuilder(), createUser());
         System.out.println(userDTO);
 
     }
--- src/main/java/creational/builder/UserWebDTO.java
+++ src/main/java/creational/builder/UserWebDTO.java
@@ -1,29 +1,41 @@
 package creational.builder;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class UserWebDTO implements UserDTO {
 
-    private final String name;
+    private String name;
 
-    private final String address;
+    private String address;
 
-    private final String age;
+    private String age;
 
-    public UserWebDTO(String name, String address, String age) {
-        this.name = name;
-        this.address = address;
-        this.age = age;
-    }
-
+    @Override
     public String getName() {
         return name;
     }
 
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
     public String getAddress() {
         return address;
     }
 
+    private void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
     public String getAge() {
         return age;
+    }
+
+    private void setAge(String age) {
+        this.age = age;
     }
 
     @Override
@@ -31,5 +43,56 @@ public class UserWebDTO implements UserDTO {
         return "name=" + name + "\nage=" + age + "\naddress=" + address ;
     }
 
+    public static UserWebDTOBuilder getBuilder(){
+        return new UserWebDTOBuilder();
+    }
+
+    public static class UserWebDTOBuilder implements  UserDTOBuilder{
+
+        private String firstName;
+        private String lastName;
+        private String age;
+        private String address;
+        private UserWebDTO userWebDTO;
+
+        @Override
+        public UserDTOBuilder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        @Override
+        public UserDTOBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        @Override
+        public UserDTOBuilder birthday(LocalDate birthday) {
+            Period age = Period.between(birthday, LocalDate.now());
+            this.age = Integer.toString(age.getYears());
+            return this;
+        }
+
+        @Override
+        public UserDTOBuilder address(Address address) {
+            this.address = address.toString();
+            return this;
+        }
+
+        @Override
+        public UserDTO build() {
+            userWebDTO = new UserWebDTO();
+            userWebDTO.setName(firstName + " " + lastName);
+            userWebDTO.setAge(age);
+            userWebDTO.setAddress(address);
+            return userWebDTO;
+        }
+
+        @Override
+        public UserDTO getUserDTO() {
+            return userWebDTO;
+        }
+    }
 
 }
